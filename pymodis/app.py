4f72011449569050a3dd3aa4cfd8d514022a2f2b
dookda <sakda.homhuan@gmail.com>
2022-07-25 15:51:31 +0700

    update

--- pymodis/app.py
+++ pymodis/app.py
@@ -33,23 +33,37 @@ def download():
     mod = f"modis_download.py -U {username} -P {password} -r -s MOLT -p MOD09A1.006 -t h27v07 -f 2022-07-01 -e 2022-07-31 {path}"
     # os.system(mod)
     myd = f"modis_download.py -U {username} -P {password} -r -s MOLA -p MYD09A1.006 -t h27v07 -f 2022-07-01 -e 2022-07-31 {path}"
-    print("finish download ", myd)
+
     os.system(myd)
+    print("finish download ", myd)
 
     # warp()
 
 
 def download2():
-    mod = f"wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD02HKM/2022/001/' --header 'Authorization: {token}' -P ."
+    url = f'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD02HKM/2022/001/'
+    mod = f"wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 '{url}' --header 'Authorization: Bearer {token}' -P ."
     os.system(mod)
 
 
 def check_extent():
-    dt = date.today()
+    # dt = date.today()
+    dt = '2022-07-24'
     url = f'https://ladsweb.modaps.eosdis.nasa.gov/archive/geoMeta/61/TERRA/2022/MOD03_{dt}.txt'
     mod = f"wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 '{url}' --header 'Authorization: Bearer {token}' -O ./txt/MOD03_{dt}.txt"
-    os.system(mod)
+    # os.system(mod)
+
+    lines = []
+    with open(f'./txt/MOD03_{dt}.txt') as f:
+        lines = f.readlines()
+
+    for l in lines:
+        arr = l.split(',')
+        arrsel = arr[9:]
+        for a in arrsel:
+            a.strip("\n")
+            print(a)
 
 
 if __name__ == '__main__':
-    check_extent()
+    download()
