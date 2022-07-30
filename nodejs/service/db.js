3e71578bec82a2b44a201b0d45b030d49f85b6d5
dookda <sakda.homhuan@gmail.com>
2022-07-30 22:39:26 +0700

    update db

--- nodejs/service/db.js
+++ nodejs/service/db.js
@@ -4,8 +4,8 @@ const { Client } = require('pg')
 // docker in localhost
 const db = new Pool({
     user: 'postgres',
-    host: 'postgres',
-    database: 'cmu_commu',
+    host: 'postgis_pymodis',
+    database: 'geodb',
     password: '1234',
     port: 5432,
 });
