f91649d33086d185193678c964100608d8284fc3
dookda <sakda.homhuan@gmail.com>
2022-08-04 23:11:32 +0700

    update

---
+++ pymodis/auth.py
@@ -0,0 +1,14 @@
+conn = {
+    "dbName": "geodb",
+    "dbUser": "postgres",
+    "dbPass": "1234",
+    "dbHost": "pymodis_postgis",
+    "dbPort": "5432",
+}
+
+earth = {
+    "user": "sakda.homhuan@gmail.com",
+    "pass": "nasaDa45060071"
+}
+
+token = 'c2FrZGE6YzJGclpHRXVhRzl0YUhWaGJrQm5iV0ZwYkM1amIyMD06MTY1ODc1MzE5Nzo4MGM0Y2YwYTkyNTk5OWFhZmQxMTBkZjI3ZTcyMDUwY2Q3MjEzNDc3'
