c40a53b68f3b83d062f7fa6fed2f7eca723661d4
dookda <sakda.homhuan@gmail.com>
2022-08-06 21:34:09 +0700

    update web

--- nodejs/www/app.js
+++ nodejs/www/app.js
@@ -1,5 +1,7 @@
+let geoserver = "http://150.95.80.114:8080/geoserver"
+
 const map = L.map('map', {
-    center: [17.848925397129943, 100.33221166785994],
+    center: [17.868925397129943, 100.31221166785994],
     zoom: 11
 })
 
@@ -9,7 +11,7 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     lyr: 'basemap'
 });
 
-const CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+const CartoDB = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 19,
@@ -18,60 +20,71 @@ const CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_al
 
 const grod = L.tileLayer('https://{s}.google.com/vt/lyrs=r&x={x}&y={y}&z={z}', {
     maxZoom: 20,
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-    lyr: 'basemap'
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 });
 
 const ghyb = L.tileLayer('https://{s}.google.com/vt/lyrs=y,m&x={x}&y={y}&z={z}', {
     maxZoom: 20,
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-    lyr: 'basemap'
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 });
 
-var ndvi = L.tileLayer.wms("http://150.95.80.114:8080/geoserver/wms?", {
-    layers: 'ndvi:ndvi_20220101',
+var prov = L.tileLayer.wms("https://rti2dss.com/geoserver/th/wms?", {
+    layers: 'th:province_4326',
     format: 'image/png',
     transparent: true,
-    styles: 'ndvi',
-    name: "ndvi"
+    CQL_FILTER: 'pro_code=53'
 });
 
-const baseMaps = {
-    "แผนที่ osm": osm,
-    "แผนที่ CartoDB": CartoDB_Positron.addTo(map),
-    "แผนที่ google road": grod,
-    "แผนที่ google hybride": ghyb
+const amp = L.tileLayer.wms("https://rti2dss.com/geoserver/th/wms?", {
+    layers: 'th:amphoe_4326',
+    format: 'image/png',
+    transparent: true,
+    CQL_FILTER: 'pro_code=53'
+});
+
+const tam = L.tileLayer.wms("https://rti2dss.com/geoserver/th/wms?", {
+    layers: 'th:tambon_4326',
+    format: 'image/png',
+    transparent: true,
+    CQL_FILTER: 'pro_code=53'
+});
+
+const baseMap = {
+    "OSM": osm,
+    "แผนที่ CartoDB": CartoDB.addTo(map),
+    "แผนที่ถนน": grod,
+    "แผนที่ภาพถ่าย": ghyb
 }
 
-var overlayMaps = {
-    "ndvi": ndvi.addTo(map)
+const overlayMap = {
+    "ขอบจังหวัด": prov,
+    "ขอบอำเภอ": amp.addTo(map),
+    "ขอบตำบล": tam.addTo(map)
 }
 
-let ndviItem = document.getElementById('ndvidate')
+L.control.layers(baseMap, overlayMap).addTo(map);
+
+let wmsList = [];
 axios.get('/api/listndvi').then((res) => {
     res.data.layers.layer.forEach(i => {
-        // console.log(i.name);
-        const nane = i.name.split(":")
-        ndviItem.innerHTML += `<option value="${nane[1]}">${nane[1]}</option>`
+        wmsList.push(i.name);
     });
 });
 
-
-L.control.layers(baseMaps, overlayMaps).addTo(map)
-
-function removeLayer() {
-
-}
-
-async function addLayer(e) {
+let wmsLyr = [];
+const addLayer = async (e) => {
+    wmsLyr = [];
+    document.getElementById("datefocus").value = e.target.value;
+    let dtxt = "ndvi_" + e.target.value.replace("-", "").replace("-", "");
+    wmsLyr.push(`ndvi:${dtxt}`)
     await map.eachLayer(i => {
         if (i.options.name == "ndvi") {
             map.removeLayer(i)
         }
     })
 
-    let a = await L.tileLayer.wms("http://150.95.80.114:8080/geoserver/wms?", {
-        layers: 'ndvi:' + e.target.value,
+    let lyr = await L.tileLayer.wms(geoserver + "/wms?", {
+        layers: 'ndvi:' + dtxt,
         format: "image/png",
         transparent: true,
         styles: "ndvi",
@@ -79,7 +92,143 @@ async function addLayer(e) {
         name: "ndvi"
     })
 
-    a.addTo(map)
+    if (wmsList.includes(wmsLyr[0])) {
+        lyr.addTo(map)
+    }
 }
 
+let ndviItem = document.getElementById('ndvidate');
 ndviItem.onchange = addLayer;
+
+var dom = document.getElementById('chart');
+var echart = echarts.init(dom, null, {
+    renderer: 'canvas',
+    useDirtyRect: false
+});
+
+// option = {
+//     title: {
+//         text: 'NDVI 2022'
+//     },
+//     tooltip: {
+//         trigger: 'axis',
+//         axisPointer: {
+//             type: 'cross',
+//             label: {
+//                 backgroundColor: '#6a7985'
+//             }
+//         }
+//     },
+//     xAxis: {
+//         type: 'category',
+//         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+//         name: 'Date of Year (DOY)',
+//         nameLocation: 'center',
+//         nameGap: 30,
+//     },
+//     yAxis: {
+//         type: 'value',
+//         name: 'NDVI',
+//         nameLocation: 'center',
+//         nameGap: 30,
+//     },
+//     series: [
+//         {
+//             data: [820, 932, 901, 934, 1290, 1330, 1320],
+//             type: 'line',
+//             smooth: true,
+//             symbol: 'none',
+//         }
+//     ]
+// };
+
+var option = {
+
+    xAxis: {
+        type: 'category',
+        name: 'Date',
+        nameLocation: 'center',
+        nameGap: 30,
+    },
+    yAxis: {
+        type: 'value',
+        max: 1,
+        name: 'NDVI',
+        nameLocation: 'center',
+        nameGap: 30,
+    },
+    series: [
+        {
+            // data: [120],
+            type: 'bar',
+            showBackground: true,
+            backgroundStyle: {
+                color: 'rgba(180, 180, 180, 0.2)'
+            }
+        }
+    ]
+};
+
+if (option && typeof option === 'object') {
+    echart.setOption(option);
+}
+
+window.addEventListener('resize', echart.resize);
+
+map.on("click", async (e) => {
+    let lyrs = wmsLyr.toString();
+    let pnt = await map.latLngToContainerPoint(e.latlng, map.getZoom());
+    let size = await map.getSize();
+    let bbox = await map.getBounds().toBBoxString();
+    let datefocus = document.getElementById("datefocus").value
+
+    let lyrInfoUrl = geoserver + "/wms?SERVICE=WMS" +
+        "&VERSION=1.1.1&REQUEST=GetFeatureInfo" +
+        "&QUERY_LAYERS=" + lyrs +
+        "&LAYERS=" + lyrs +
+        "&Feature_count=300" +
+        "&INFO_FORMAT=application/json" +
+        "&X=" + Math.round(pnt.x) +
+        "&Y=" + Math.round(pnt.y) +
+        "&SRS=EPSG:4326" +
+        "&WIDTH=" + size.x +
+        "&HEIGHT=" + size.y +
+        "&BBOX=" + bbox;
+
+    let xAxis = [];
+    let series = [];
+    await axios.get(lyrInfoUrl).then(async (r) => {
+        if (r.data.features) {
+            r.data.features.map(async (i, k) => {
+                document.getElementById("ndvitxt").innerHTML = `<div class="desc">lat: ${e.latlng.lat} 
+                <br>lon: ${e.latlng.lng}
+                <br>NDVI: ${i.properties.GRAY_INDEX.toFixed(3)} 
+                <br>วันที่: ${datefocus}`;
+                xAxis.push(k);
+                series.push(i.properties.GRAY_INDEX.toFixed(3));
+            })
+
+            await echart.setOption({
+                xAxis: {
+                    type: 'category',
+                    data: [datefocus],
+                },
+                yAxis: {
+                    type: 'value',
+                    max: 1,
+                },
+                series: [
+                    {
+                        data: series,
+                        type: 'bar',
+                        showBackground: true,
+                        backgroundStyle: {
+                            color: 'rgba(180, 180, 180, 0.2)'
+                        }
+                    }
+                ]
+            })
+        }
+    })
+})
+
