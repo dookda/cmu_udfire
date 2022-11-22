8d3334b8be9f7b79131d9e2456326ca0df2b79f6
sakda homhuan <sakda.homhuan@gmail.com>
2022-11-22 21:58:42 +0700

    update hotspot

--- nodejs/www/app.js
+++ nodejs/www/app.js
@@ -70,7 +70,9 @@ const baseMap = {
     "แผนที่ภูมิประเทศ": gter
 }
 
+var fc = L.featureGroup()
 const overlayMap = {
+    "จุดความร้อนปัจจุบัน": fc.addTo(map),
     "ขอบจังหวัด": prov,
     "ขอบอำเภอ": amp.addTo(map),
     "ขอบตำบล": tam.addTo(map)
@@ -489,7 +491,48 @@ let showIndx = async (e) => {
     })
 }
 
+let hpData = axios.get("https://firms.modaps.eosdis.nasa.gov/mapserver/wfs/SouthEast_Asia/c56f7d70bc06160e3c443a592fd9c87e/?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAME=ms:fires_snpp_24hrs&STARTINDEX=0&COUNT=5000&SRSNAME=urn:ogc:def:crs:EPSG::4326&BBOX=-90,-180,90,180,urn:ogc:def:crs:EPSG::4326&outputformat=geojson");
+let onEachFeatureHotspot = (feature, layer) => {
+    if (feature.properties) {
+        layer.bindPopup(
+            `<span class="kanit"><b>ตำแหน่งจุดความร้อน</b>
+            <br/>ข้อมูลจาก VIIRS
+            <br/>ตำแหน่งที่พบ : ${feature.properties.latitude}, ${feature.properties.longitude} 
+            <br/>ค่า Brightness temperature: ${feature.properties.brightness} Kelvin
+            <br/>วันที่: ${feature.properties.acq_datetime} UTC`
+        );
+    }
+}
+
+let loadHotspot = async () => {
+    let hp = await hpData;
+    const fs = hp.data.features;
+    var geojsonMarkerOptions = {
+        radius: 6,
+        fillColor: "#ff5100",
+        color: "#a60b00",
+        weight: 0,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    await L.geoJSON(fs, {
+        filter: function (feature) {
+            if (feature.geometry.coordinates[0] > 96.295861 && feature.geometry.coordinates[0] < 106.113154) {
+                if (feature.geometry.coordinates[1] > 5.157973 && feature.geometry.coordinates[1] < 20.221918) {
+                    myModal.hide();
+                    return feature
+                }
+            }
+        },
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        },
+        name: "lyr",
+        onEachFeature: onEachFeatureHotspot
+    }).addTo(fc)
+}
+
 map.on("click", async (e) => {
     showIndx(e)
 })
-
