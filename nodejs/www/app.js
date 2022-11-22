991236a598a8a7f3cefcac568b12a24f8b04995e
sakda homhuan <sakda.homhuan@gmail.com>
2022-11-22 20:26:12 +0700

    update invese data

--- nodejs/www/app.js
+++ nodejs/www/app.js
@@ -366,7 +366,7 @@ let showForestIndx = async (indx, ndviItem) => {
 }
 
 let fireIntensity = (ndvi) => {
-    let M = ((10007 * ndvi) - 2936.8) / 1000000   // biomass
+    let M = ((10007 * (ndvi * -1) - 2936.8) / 1000000   // biomass
     let h = 19580                       // kj/kg
     let a = 1.5
     let b = 1.2
